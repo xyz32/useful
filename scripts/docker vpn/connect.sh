28f8b1b5f799467339926d677a1f58955b04cca8
xyz32 <xyz32@users.noreply.github.com>
2020-02-13 15:52:20 -0500

    Add command to route everithing through tunnel

--- scripts/docker vpn/connect.sh
+++ scripts/docker vpn/connect.sh
@@ -2,3 +2,6 @@
 
 #openconnect
 openconnect [server_URL] --csd-wrapper /usr/libexec/openconnect/csd-wrapper.sh
+
+#route all traffic through tunnel:
+#openconnect [server_URL] --csd-wrapper /usr/libexec/openconnect/csd-wrapper.sh --script 'unset CISCO_SPLIT_INC; /etc/vpnc/vpnc-script'
