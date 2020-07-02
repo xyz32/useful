9009ecbaf058180a2dcc49fe94e577aa478aca06
xyz32 <xyz32@users.noreply.github.com>
2020-07-02 08:02:22 -0400

    Make the script search in subfolders as well.

--- scripts/multi media/convert.sh
+++ scripts/multi media/convert.sh
@@ -2,16 +2,33 @@
 
 set -x
 
-pushd "${1}"
+function convert() {
+    pushd "${1}"
+    
+    FILES=$(find . -type f -iname '*.MOV' |sort|uniq)
 
-for fullFileName in ./*.MOV; do
-  filename=$(basename "$fullFileName")
-  extension="${filename##*.}"
-  filename="${filename%.*}"
-  if [ ! -f ./${filename}.avi ]
-  then
-    mencoder "./${fullFileName}" -o "./${filename}.avi" -oac mp3lame -ovc x264
-  fi
+    IFS="\n"
+    for fullFileName in ${FILES}
+    do
+        filename=$(basename "${fullFileName}")
+        extension="${filename##*.}"
+        filename="${filename%.*}"
+        mencoder "${fullFileName}" -o "${filename}.avi" -oac mp3lame -ovc x264
+        mv "${fullFileName}" "${fullFileName}.done"
+    done
+
+    popd
+}
+
+OIFS=$IFS
+
+FOLDERS=$(find "${1}" -type f -iname '*.MOV' -printf '%h\n'|sort|uniq)
+IFS="\n"
+for folder in ${FOLDERS}
+do
+    echo "===============================================> Converting: ${folder}"
+    convert "${folder}"
 done
 
-popd
+IFS=$OIFS
+
